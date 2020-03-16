4fa0e7bbd93c316a2367643e1e293349eb985896
tarunanandr <tarunanand.chatur@gmail.com>
2020-03-16 16:40:22 +0530

    Routes altered to only use todos resource

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,16 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:id).map { |todo| todo.to_pleasant_string }.join("\n")
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo=Todo.create!( todo_text: todo_text, due_date: due_date, completed: false)
+    confirmation_text = "Todo created with id = #{new_todo.id}"
+    render plain: confirmation_text
   end
 
   def show
