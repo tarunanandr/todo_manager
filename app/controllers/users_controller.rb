02f88e587de8dec29e5ddc411c6ae60ae3d2f5f4
tarunanandr <tarunanand.chatur@gmail.com>
2020-03-26 16:00:47 +0530

    Create method added for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,14 @@ class UsersController < ApplicationController
     render plain:
       "User Details\n\n"+User.all.map {|user| user.to_displayable_content}.join("\n")
   end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name, email: email, password: password)
+    confirmation_text = "User created with id #{new_user.id}"
+    render plain: confirmation_text
+  end
+
 end
