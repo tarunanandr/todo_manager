6f25fb724a071e04c3e3a52876da982d57dc24eb
tarunanandr <tarunanand.chatur@gmail.com>
2020-04-06 12:56:48 +0530

    Updated DataBase Migrations

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,19 +2,21 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render plain: Todo.order(:id).map { |todo| todo.to_pleasant_string }.join("\n")
+    #render plain: Todo.order(:id).map { |todo| todo.to_pleasant_string }.join("\n")
+    render "index"
   end
 
   def create
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
-    new_todo=Todo.create!( todo_text: todo_text, due_date: due_date, completed: false)
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
     confirmation_text = "Todo created with id = #{new_todo.id}"
     render plain: confirmation_text
   end
 
   def show
     id = params[:id]
+    todo = Todo.find(id)
     render plain: Todo.find(id).to_pleasant_string
   end
 
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,48 @@
+require "active_record"
+
 class Todo < ActiveRecord::Base
   def to_pleasant_string
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:short)} #{todo_text} #{is_completed}"
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.duetoday
+    where("due_date = ?", Date.today)
+  end
+
+  def self.duelater
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    where("completed = ?", true)
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+    puts "Overdue\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+    puts "Due Today\n"
+    puts duetoday.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+    puts "Due Later\n"
+    puts duelater.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+
+  def self.add_task(h)
+    create!(todo_text: h[h.keys[0]], due_date: Date.today + h[h.keys[1]], completed: false)
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = find(todo_id)
+    todo.completed = true
+    todo.save
+    return todo
   end
 end
