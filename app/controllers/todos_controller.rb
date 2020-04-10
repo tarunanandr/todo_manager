52e1251c65e4d54799c40781292b5834cc8cb193
tarunanandr <tarunanand.chatur@gmail.com>
2020-04-10 14:08:39 +0530

    Completed action through checkbox implemented

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,8 +10,9 @@ class TodosController < ApplicationController
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    confirmation_text = "Todo created with id = #{new_todo.id}"
-    render plain: confirmation_text
+    "" 'confirmation_text = "Todo created with id = #{new_todo.id}"
+    render plain: confirmation_text' ""
+    redirect_to todos_path
   end
 
   def show
@@ -26,6 +27,7 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "Updated the Todo with completed value = #{completed}"
+    "" 'render plain: "Updated the Todo with completed value = #{completed}"' ""
+    redirect_to todos_path
   end
 end
