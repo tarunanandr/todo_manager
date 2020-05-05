6cec13247f7fc450495f3db27af111876357a4d4
tarunanandr <tarunanand.chatur@gmail.com>
2020-05-05 22:14:20 +0530

    Configurations to add to Heroku

--- db/migrate/20200505162020_create_todos.rb
+++ db/migrate/20200505162020_create_todos.rb
@@ -1,6 +1,7 @@
 class CreateTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :todos do |t|
+      t.bigint :user_id
       t.text :todo_text
       t.date :due_date
       t.boolean :completed
